21151269bd7a63fe55342df72d5c0d47578a475b
Oleksandr Poltavets <sancho.ck@gmail.com>
2018-11-27 22:44:26 +0200

    added new method to user

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+  attr_reader :first_name, :last_name, :email
+
   validates_uniqueness_of :email
   has_secure_password
 
   has_many :memberships
   has_many :groups, through: :memberships
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
